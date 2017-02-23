def93fb3300a4c8d2581b2e8eede8835efd87750
jr55662003 <jr55662013@gmail.com>
2017-02-23 19:05:54 +0800

    HW0

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -4,13 +4,15 @@ import sys
 
 im1 = Image.open(sys.argv[1])
 im2 = Image.open(sys.argv[2])
-result = np.zeros((512,512,4)) 
+width, height = im1.size
+result = np.zeros((height, width, 4), dtype=np.uint8) 
 
-for i in range(512):
-    for j in range(512):
+for i in range(width):
+    for j in range(height):
+        
         if im1.getpixel((i,j)) != im2.getpixel((i,j)):
             result[j][i] = im2.getpixel((i,j))
-
+            
 result = result.astype(np.uint8)
 result = Image.fromarray(result, "RGBA")
 result.save("ans_two.png")
