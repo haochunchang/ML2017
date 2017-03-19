4cbb614de966e7ba6ec8fbd7cb639277a25f60b4
jr55662003 <jr55662013@gmail.com>
2017-03-19 11:47:03 +0800

    Matrix operation to speed up batch gradient descent

--- hw1/lr_best.py
+++ hw1/lr_best.py
@@ -4,7 +4,7 @@ import sys, random, pickle
 import feature
 import matplotlib.pyplot as plt
 
-def train_lr(train, iternum=1):
+def train_lr(train, iternum=1, lamb=1e-4):
     '''
     Training W using train_data in train
     
@@ -12,37 +12,52 @@ def train_lr(train, iternum=1):
     model: # of order of the regression line
     Return trained W matrix
     '''
+    
     # Model initialization 
-    W = np.zeros((19,9))
+    W = np.zeros(190,)
+    y = []
+    X = []
     std = train.labelstd
     mu = train.labelmu
     
-    lr = 1e-4
+    for i in range(len(train)):
+        y.append(train.get_label(i))
+        X.append(train[i]) 
+    X = np.array(X, dtype=np.float64)
+    y = np.array(y, dtype=np.float64)
+    
+    lr = 1e-1
     pre_grad = 0
-    rho = 0.9 # decaying coefficient
     iteration = iternum
     for epoch in range(1, iteration+1):    
         grad = 0
         loss = 0
         total_loss = 0
-        train = train.shuffle()
         # Run through all training data
-        for i in range(len(train)):
-            y_hat = train.get_label(i)
-            y = np.multiply(W, train[i]).sum()  
-            grad = (2 * ((y - y_hat)) * train[i]) / len(train) 
-            loss = ((y - y_hat) ** 2) / len(train)
-             
-            pre_grad = rho * pre_grad + (1 - rho) * (grad ** 2)
-            rms_g = np.sqrt(pre_grad + 10e-8)
+        y_hat = np.dot(X, W)
+        delta = y_hat - y    
+        loss = np.dot(delta, delta.T) + lamb * np.dot(W, W.T)     
+        grad = 2 * (np.dot(X.T, delta) + lamb * W)
+ 
+        pre_grad += grad ** 2
+        rms_g = np.sqrt(pre_grad + 1e-8)
             
-            # Update parameters
-            W -= (lr / rms_g) * grad
-            total_loss += loss
-        total_loss = np.sqrt(total_loss * std + mu)
-        #print(total_loss)
+        # Update parameters
+        W -= (lr / rms_g) * grad
+        rmse = np.sqrt(loss / len(train))
+        print(rmse.sum())
     
-    print("Training loss: %f" % total_loss) 
+    print("Training loss: %f" % rmse.sum()) 
+    '''
+    XTX = np.linalg.pinv(np.matmul(X.T, X))
+    W = np.matmul(np.matmul(XTX, X.T), y)
+    y_hat = np.dot(X, W)    
+
+    loss = ((y_hat - y) ** 2) / len(train)    
+    rmse = np.sqrt(loss)
+    
+    print("Training loss: %f" % rmse.sum()) 
+    '''
     return W
  
 def test_lr(W, test, outfilepath, train):
@@ -50,11 +65,16 @@ def test_lr(W, test, outfilepath, train):
     Use trained W to predict test_X.csv and output result file.
     '''
     test = feature.TestFeature(test, train.mu, train.std)
+    test = test.scaling()
+    test.add_bias()
     with open(outfilepath, 'w') as o:
         o.write("id,value\n")
         for i in range(len(test)):
-            y = np.multiply(W, test[i]).sum() * train.labelstd + train.labelmu    
-            y = str(y)
+            total_y = 0
+            for j in range(len(W)):
+                total_y += np.dot(test[i], W[j])
+            y = total_y / len(W)   
+            y = str(float(y * train.labelstd + train.labelmu))
             o.write("id_"+str(i)+","+y)
             o.write("\n")
     print("Testing result stored in %s" % outfilepath)
@@ -68,7 +88,7 @@ def validate(W, val_f, val_l, train):
     total_loss = loss * train.labelstd + train.labelmu
     print("Validation loss: %f" % total_loss)
     return total_loss
-        
+ 
 def lr_main(train, test, outfilepath):
     '''
     Linear Regression main function
@@ -77,32 +97,26 @@ def lr_main(train, test, outfilepath):
     test: filepath of test_X.csv
     outfilepath: filepath of predicted_result.csv
     '''
-    
+    # Load in data & preprocessing
     training_data = pd.read_csv(train, sep=",", encoding="big5") 
-    val_err = 10e8
-    epoch = []
-    err = []
-    for i in range(200, 1200, 100):
+
+    # Training
+    W_best = []
+    for i in range(1):
         train = feature.Feature(training_data)
-        val_f, val_l = train.sample_val(len(train) // 10)    
-        W = train_lr(train, iternum=i)
-        loss = validate(W, val_f, val_l, train)
-        if loss < val_err:
-            val_err = loss
-            W_best = W
-        err.append(loss)
-        epoch.append(i)    
-
-    plt.scatter(epoch, err)
-    plt.show()
-
-    with open("./model/W_epoch.pkl", "wb") as o:
+        train = train.scaling()
+        train.add_bias()
+        #train, _ = train.bagging(train)
+        W = train_lr(train, iternum=50000)
+        W_best.append(W)
+    
+    with open("./model/W_best.pkl", "wb") as o:
         pickle.dump(W_best, o)
     
     # Testing and output result
     test = pd.read_csv(test, sep=",", header=None)
     test_lr(W_best, test, outfilepath, train)
-
+    
 if __name__ == "__main__":
     train = sys.argv[1]
     test = sys.argv[2]
