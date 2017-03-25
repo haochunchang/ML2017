3ecfc24e4b7a32d5588520d9fc9a26ec246340b2
jr55662003 <jr55662013@gmail.com>
2017-03-25 19:11:01 +0800

    Try my feature encoding

--- hw2/my_logistic.py
+++ hw2/my_logistic.py
@@ -3,6 +3,49 @@ import numpy as np
 import sys, pickle
 import feature
 
+def logi_main(raw_train, raw_test, outfilepath):
+    
+    # Loading in Training data
+    train_data = pd.read_csv(raw_train, header=None)
+
+    # Extract Labels (32561,)
+    label_extract = lambda x: 1 if x else 0
+    labels = np.array([label_extract(x == ' >50K') for x in train_data.iloc[:, 14]])
+
+    # Hashing base categorical features
+    ''' 
+    train = feature.Xfeature(train_data, labels).preprocess()
+    
+    # Normalization
+    train = train.normalize()
+    train.add_bias()  
+    
+    # Model initialization
+    W = np.zeros((107,))    
+
+    # Training
+    W_trained = logi_train(W, train_data, labels, epoch=1000, batch_size=20, lr=1e-3)
+    
+    # Save model
+    with open("./model/W_my_logistic.pkl", "wb") as o:
+        pickle.dump(W_trained, o)
+    with open("./model/W_my_logistic.csv", "w") as o:
+        for W in W_trained:
+            o.write(str(W))
+            o.write("\n")
+    
+    # Loading in testing data
+    test_data = pd.read_csv(xtest)
+    test = feature.Xfeature(test_data, None).preprocess()
+    
+    # Normalization
+    test = test.normalize()
+    test.add_bias()
+
+    # Testing and output prediction file
+    logi_test(W_trained, xtest, outfilepath)
+    '''
+
 def activate(x):
     sigmoid = 1.0 / (1.0+np.exp(-x))
     return (sigmoid >= 0.5).astype(np.int)
@@ -13,12 +56,6 @@ def dsigmoid(x):
  
 def logi_train(W, train_data, labels, epoch=1000, batch_size=20, lr=1e-1, lamb=1e-3):
     
-    train = feature.Xfeature(train_data, labels).preprocess()
-    
-    # Normalization
-    train = train.normalize()
-    train.add_bias()  
-
     nbatch = len(train) // batch_size + 1
     pre_grad = 0
     rho = 0.9
@@ -39,16 +76,8 @@ def logi_train(W, train_data, labels, epoch=1000, batch_size=20, lr=1e-1, lamb=1
 
     return W 
 
-def logi_test(W, xtest, outfilepath):
+def logi_test(W, test, outfilepath):
     
-    # Loading in testing data
-    test_data = pd.read_csv(xtest)
-    test = feature.Xfeature(test_data, None).preprocess()
-    
-    # Normalization
-    test = test.normalize()
-    test.add_bias()
-
     with open(outfilepath, 'w') as o:
         o.write("id,label\n")
         for i in range(len(test)):
@@ -57,32 +86,8 @@ def logi_test(W, xtest, outfilepath):
             o.write("\n")
     print("Testing result stored in %s" % outfilepath)
      
-def logi_main(xtrain, ytrain, xtest, outfilepath):
-    
-    # Loading in Training data
-    train_data = pd.read_csv(xtrain)
-    labels = pd.read_csv(ytrain, header=None)
-    
-    # Model initialization
-    W = np.zeros((107,))    
-
-    # Training
-    W_trained = logi_train(W, train_data, labels, epoch=1000, batch_size=20, lr=1e-3)
-    
-    # Save model
-    with open("./model/W_logistic_X.pkl", "wb") as o:
-        pickle.dump(W_trained, o)
-    with open("./model/W_logistic_X.csv", "w") as o:
-        for W in W_trained:
-            o.write(str(W))
-            o.write("\n")
-       
-    # Testing and output prediction file
-    logi_test(W_trained, xtest, outfilepath)
-
 if __name__ == "__main__":
-    xtrain = sys.argv[1]
-    ytrain = sys.argv[2]
-    xtest = sys.argv[3]
-    outfilepath = sys.argv[4]
-    logi_main(xtrain, ytrain, xtest, outfilepath)
+    raw_train = sys.argv[1]
+    raw_test = sys.argv[2]
+    outfilepath = sys.argv[3]
+    logi_main(raw_train, raw_test, outfilepath)
