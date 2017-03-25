3911498ca2cdfa054ae03530a85d3b03c3b4d2b5
jr55662003 <jr55662013@gmail.com>
2017-03-25 19:10:17 +0800

    Add bucketize

--- hw2/feature.py
+++ hw2/feature.py
@@ -28,6 +28,42 @@ class Xfeature(object):
         self.__label = np.array(self.__label).flatten()
         return self     
 
+    def __hash(self, vector):
+        '''
+        Input a vector of continuous feature
+        Convert it to binned number.
+        (e.g. 17~27: 1, 27~37: 2,...)
+        '''
+        vmax = int(vector.max())
+        vmin = int(vector[vector > 0].min())
+        bin_size = (vmax - vmin) // 5
+        nbin = 5
+        
+        for i in range(len(vector)):
+            if vector[i] < vmin+bin_size:
+                vector[i] = 1
+            elif (vector[i] >= vmin+bin_size and vector[i] < vmin+bin_size*2):
+                vector[i] = 2
+            elif vmin+bin_size <= vector[i] and vector[i]< vmin+bin_size*3:
+                vector[i] = 3
+            elif vmin+bin_size <= vector[i] and vector[i]< vmin+bin_size*4:
+                vector[i] = 4
+            else:
+                vector[i] = 5
+
+        return vector
+
+
+    def bucketize(self):
+        '''
+        Bucketize continuous feature into catergorical feature.
+        Hash each bucket
+        '''
+        for i in [0, 1, 3, 4, 5]:
+            self.__data[:, i] = self.__hash(self.__data[:, i])
+
+        return self
+
     def normalize(self):
         fmax = (self.__data).max(axis=0)
         for i in range(len(fmax)):
@@ -58,7 +94,6 @@ class Xfeature(object):
         ''' 
         pre_seed = []
         seed = [i for i in np.random.randint(0, self.__data.shape[0], size=size) if i not in pre_seed]
-
         batch_x = self.__data[seed, :]
         batch_y = self.__label[seed]
         
