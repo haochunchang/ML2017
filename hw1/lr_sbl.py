53daba38784d53e81dfd0f92270e9f90044a35fa
haochunchang <b03b02014@ntu.edu.tw>
2017-04-16 21:46:21 +0800

    comment out matplotlib

--- hw1/lr_sbl.py
+++ hw1/lr_sbl.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import sys, random, pickle
 import feature
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 '''
 def test_lr(W, test, outfilepath, train):
