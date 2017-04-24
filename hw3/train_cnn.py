336020abeef3b78719a6ad48e0e3646019ec015f
haochunchang <b03b02014@ntu.edu.tw>
2017-04-24 16:37:11 +0800

    Model for report

--- hw3/train_cnn.py
+++ hw3/train_cnn.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys, csv, pickle
-import feature
+from utils import feature, utils
 from PIL import Image
 
 import keras
@@ -12,8 +12,21 @@ from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint 
 from keras import backend as K
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, Callback
 K.set_image_dim_ordering('tf')
+
+class History(Callback):
+    def on_train_begin(self,logs={}):
+        self.tr_losses=[]
+        self.val_losses=[]
+        self.tr_accs=[]
+        self.val_accs=[]
+
+    def on_epoch_end(self,epoch,logs={}):
+        self.tr_losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.tr_accs.append(logs.get('acc'))
+        self.val_accs.append(logs.get('val_acc'))
 
 def cnn_train(train_filepath, batch_size=128, epochs=10, data_augmentation=False, pretrained=None):
 
@@ -28,10 +41,10 @@ def cnn_train(train_filepath, batch_size=128, epochs=10, data_augmentation=False
     #with open('validation_data_augmented.pkl', 'wb') as v:
     #    pickle.dump((x_test, y_test, seed), v)
     
-    with open('validation_data.pkl', 'rb') as v:
-        x_test, y_test = pickle.load(v)
-
-    train_norm = train_norm.delete(x_test, y_test) # Do not cancel out validation set!
+    with open('validation_data_augmented.pkl', 'rb') as v:
+        x_test, y_test, seed = pickle.load(v)
+    
+    x_train = train_norm.delete(seed)
     x_train = train_norm.get_feature()
     y_train = keras.utils.to_categorical(train_norm.get_label(), num_classes) 
     y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -97,53 +110,31 @@ def cnn_train(train_filepath, batch_size=128, epochs=10, data_augmentation=False
         print('Continue Training.')    
 
     # Train model
-    if not data_augmentation:
-        print('Not using data augmentation.')
-        checkpointer = ModelCheckpoint(filepath="./checkpoints/weights.{epoch:02d}-{val_acc:.2f}.h5", verbose=1, save_best_only=True, monitor='val_acc') 
-        cnn.fit(x_train, y_train,
-                batch_size=batch_size,
-                epochs=epochs,
-                verbose=1,
-                shuffle=True,
-                validation_data=(x_test, y_test),
-                callbacks=[TensorBoard(log_dir='./log/events.epochs'+str(epochs)), checkpointer])
-    else:
-        print('Using real-time data augmentation.')
-        checkpointer = ModelCheckpoint(filepath="./checkpoints/weights.{epoch:02d}-{val_acc:.2f}.h5", verbose=1, save_best_only=True, monitor='val_acc')  
-        datagen = ImageDataGenerator(
-            featurewise_center=False,  # set input mean to 0 over the dataset
-            samplewise_center=False,  # set each sample mean to 0
-            featurewise_std_normalization=False,  # divide inputs by std of the dataset
-            samplewise_std_normalization=False,  # divide each input by its std
-            zca_whitening=False,  # apply ZCA whitening
-            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-            horizontal_flip=True,  # randomly flip images
-            vertical_flip=False) # randomly flip images
+    print('Using real-time data augmentation.')
+    history = History()
+    checkpointer = ModelCheckpoint(filepath="./checkpoints/weights.{epoch:02d}-{val_acc:.2f}.h5", verbose=1, save_best_only=True, monitor='val_acc')  
+    datagen = ImageDataGenerator(
+                width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+                height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+                horizontal_flip=True)  # randomly flip images
 
-        datagen.fit(x_train)
-        cnn.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
-                          steps_per_epoch=x_train.shape[0] // batch_size,
-                          epochs=epochs, validation_data=(x_test, y_test),
-                          initial_epoch=29, 
-                          callbacks=[TensorBoard(log_dir='./log/events.epochs'+str(epochs)+'augmented'), checkpointer])
+    datagen.fit(x_train)
+    cnn.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                      steps_per_epoch=x_train.shape[0] // batch_size,
+                      epochs=epochs, validation_data=(x_test, y_test),
+                      callbacks=[TensorBoard(log_dir='./log/events.epochs'+str(epochs)), checkpointer, history])
 
-    return cnn
-
-def save_model(cnn, epochs):
-        
+    utils.dump_history('./log/',history)
+    
     # Serialize model weights and save them
     cnn.save_weights('models/cnn_'+str(epochs)+'.h5')
     print("CNN model saved.") 
-
+        
 def cnn_main(train_filepath):
     
-    model_filepath = 'checkpoints/weights.29-0.75.h5'
-    #model_filepath = None
-    epochs = 80 
-    cnn = cnn_train(train_filepath, batch_size=128, epochs=epochs, data_augmentation=True, pretrained=model_filepath)    
-    save_model(cnn, epochs)
+    model_filepath = None
+    epochs = 80
+    cnn_train(train_filepath, batch_size=64, epochs=epochs, data_augmentation=True, pretrained=model_filepath)    
 
 if __name__ == "__main__":
     cnn_main(sys.argv[1])
