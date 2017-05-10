6225cde943e5beea17897b60326bb60c2e616226
haochunchang <b03b02014@ntu.edu.tw>
2017-05-10 20:34:13 +0800

    Generated sample data

--- hw4/guess_dim.py
+++ hw4/guess_dim.py
@@ -1,33 +1,15 @@
-import sys, pickle
+import sys, os, pickle
 import math
 import numpy as np
-from sklearn.decomposition import KernelPCA, FactorAnalysis
-from sklearn import metrics
-from sklearn import manifold
-from sklearn import neighbors
-from sklearn.model_selection import cross_val_score, KFold
-import matplotlib.pyplot as plt
 
 def predict_dim(data, index):
 
-    portion = len(data) // 10
-    data = data[:portion]
-    logMLE = 0
-    dim = 1 
+    ref = np.loadtxt('train_mmv.txt')
+    ref = ref[:, 1] 
+    mvar = (data.std(axis=1) ** 2).mean() 
     
-    k = 20
-    nbrs = neighbors.NearestNeighbors(n_neighbors=k, algorithm='kd_tree', n_jobs=-1).fit(data)     
-    with open('./data/precomputed/index{}_{}neighbor_graph.pkl'.format(index, k), 'wb') as g:
-        pickle.dump(nbrs, g)
-    
-    Tk = nbrs.kneighbors(data)[0][:,k-1]
-    for j in range(1, k-1):
-        logMLE += np.log(Tk / nbrs.kneighbors(data)[0][:,j])
- 
-    logMLE = np.mean(logMLE / (k - 2))
-    dim = math.floor(1 / logMLE)
-    print('Best dimesion of index {} is {}.'.format(index, dim))
-
+    dim = np.absolute(ref - mvar).argmin() + 1
+    print(index, dim)
     return math.log(dim)
 
 def main(data_path, outfilepath):
