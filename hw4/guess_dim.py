e41db26e2e5d4d6c25dea0d6f83036563d02f7a2
haochunchang <b03b02014@ntu.edu.tw>
2017-05-12 22:14:56 +0800

    Method in report

--- hw4/guess_dim.py
+++ hw4/guess_dim.py
@@ -1,24 +1,40 @@
 import sys, os, pickle
 import math
 import numpy as np
+from sklearn.neighbors import NearestNeighbors as kNN
+from sklearn.svm import LinearSVR as SVR
+from generate_data import get_eigenvalues
 
-def predict_dim(data, index):
-
-    ref = np.loadtxt('train_mmv.txt')
-    ref = ref[:, 1] 
-    mvar = (data.std(axis=1) ** 2).mean() 
+def predict_dim(data):
     
-    dim = np.absolute(ref - mvar).argmin() + 1
-    print(index, dim)
-    return math.log(dim)
+    # Train model
+    npzfile = np.load('train_data.npz')
+    X = npzfile['X']
+    y = npzfile['y']
+    model = SVR(C=1.5)
+    model.fit(X, y)   
+
+    with open('model/svr.pkl', 'rb') as f:
+        model = pickle.load(f)
+    
+    # predict
+    dims = []
+    test = []
+    for i in range(200):
+        evals = get_eigenvalues(data[str(i)])
+        test.append(evals)
+    test = np.array(test)
+    dims = model.predict(test)
+
+    return dims
 
 def main(data_path, outfilepath):
     data = np.load(data_path)
-
+    dims = predict_dim(data)
+    
     with open(outfilepath, 'w') as o:
         o.write("SetId,LogDim\n")  
-        for i in range(200):
-            Logdim = predict_dim(data[str(i)], i)   
+        for i, Logdim in enumerate(dims):
             o.write(str(i)+","+str(Logdim))
             o.write("\n")
     print("Testing result stored in %s" % outfilepath)
