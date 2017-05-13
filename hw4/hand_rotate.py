e2598568dc8b0ca3a2a76c763619107011ffe3c1
haochunchang <b03b02014@ntu.edu.tw>
2017-05-13 11:25:16 +0800

    Update

--- hw4/hand_rotate.py
+++ hw4/hand_rotate.py
@@ -1,10 +1,12 @@
 import sys, os, pickle
 import glob
+import PIL
 import numpy as np
 from scipy import misc
 from sklearn.neighbors import NearestNeighbors
 from sklearn.svm import LinearSVR as SVR
 from generate_data import get_eigenvalues
+
 
 def predict_dim(data):
     
@@ -15,9 +17,8 @@ def predict_dim(data):
     # predict
     dims = []
     test = []
-    for i in range(data.shape[0]):
-        evals = get_eigenvalues(data[i])
-        test.append(evals)
+    evals = get_eigenvalues(data)
+    test.append(evals)
     test = np.array(test)
     dims = model.predict(test)
 
@@ -26,12 +27,22 @@ def predict_dim(data):
 def main():
     
     # Load in hand rotation data
-    filelist = sorted(glob.glob('./data/hand/*.png'))
+    filelist = sorted(glob.glob('./data/hand/resized/*.png'))
     image_list = [misc.imread(filename) for filename in filelist]   
-    imgs = np.array(image_list).reshape(len(image_list), 480, 512)
+    imgs = np.array(image_list).reshape(len(image_list), 32*34)
 
+    # Resized Images
+    ''' 
+    basewidth = 32
+    for i in range(len(image_list)):
+        img = PIL.Image.fromarray(imgs[i])
+        wpercent = (basewidth / float(imgs[i].shape[0]))
+        hsize = int((float(imgs[i].shape[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+        img.save('./data/hand/resized/hand_seq{}.png'.format(i+1))
+    '''
     dims = predict_dim(imgs)
     print(dims.mean())
-
+    
 if __name__ == "__main__":
     main()
