5f2aa86b9607f98ba1324cdd6a4a3f2a8b2a89dc
haochunchang <b03b02014@ntu.edu.tw>
2017-05-19 21:09:32 +0800

    Add wget

--- hw5/hw5_best.sh
+++ hw5/hw5_best.sh
@@ -1,1 +1,5 @@
+cd models
+wget https://github.com/haochunchang/ML2017/releases/download/v.0.51172-public/best_model.json
+wget https://github.com/haochunchang/ML2017/releases/download/v.0.51172-public/best.h5
+cd ../
 python3.5 test_best.py $1 $2
