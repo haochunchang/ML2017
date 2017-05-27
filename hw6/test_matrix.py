dd43bd0dc340fca558d280a3ea6994e5ffaefb03
haochunchang <b03b02014@ntu.edu.tw>
2017-05-27 20:35:41 +0800

    Base script

---
+++ hw6/test_matrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import os, sys, pickle
+import keras
+from keras.models import model_from_json
+
+   
+def main(test_filepath, outfilepath):
+
+    # Load in training data and preprocessing
+    test = pd.read_csv(test_filepath)
+    
+    users = test['UserID'].values
+    movies = test['MovieID'].values
+
+    # Load models
+    with open("models/dnn_model.json", "r") as json_file:
+        dnn = model_from_json(json_file.read())
+    dnn.load_weights('models/dnn.h5')
+
+    pred = dnn.predict([movies, users], batch_size=128, verbose=1) 
+    sub = pd.DataFrame()
+    sub['TestDataID'] = test['TestDataID']
+    sub['Rating'] = pred
+    sub.to_csv(outfilepath, index=False) 
+     
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
